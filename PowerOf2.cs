9594f276c1c8bc12c46ec0de8cae9e0e8705fff3
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-07 12:06:08 +0530

    [Shantanu] Added: 'Table of Power of 2 program.'

---
+++ PowerOf2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PowerOf2
+{
+    class PowerOf2
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Console.WriteLine("Enter value of N:");
+            N = int.Parse(Console.ReadLine());
+
+            if (0 < N)
+            {
+                Console.WriteLine("Error. Value of N should be 0 <= N < 31");
+            }
+
+
+
+            else if (N < 31)
+            {
+                int i = 0;
+                while (i <= N)
+                {
+                    i++;
+                    double table = Math.Pow(2, i);
+                    Console.WriteLine("2^N= " + table);
+                }
+            }
+
+            
+            else
+            {
+                Console.WriteLine("Error. Value of N should be 0 <= N < 31");
+            }
+        }
+    }
+}
