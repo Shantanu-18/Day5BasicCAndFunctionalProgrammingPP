61935b98a97b81242a6bb929ef0ccf6494331749
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-07 13:12:51 +0530

    [Shantanu] Added: 'Program to find even or odd number.'

---
+++ evenOrOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace evenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, answer;
+            Console.Write("Enter number that you want to check: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+
+            answer = number % 2;
+
+            if (answer == 0)
+            {
+                Console.WriteLine(+number+" is even number.");
+            }
+            else
+            {
+                Console.WriteLine(+number+" is odd number.");
+            }
+        }
+    }
+}
