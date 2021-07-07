915f88a96aa65cac00ba3cdf9fea2bf3b6a934fb
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-07 11:58:49 +0530

    [Shantanu] Added: 'Flip Coin and percentage of Heads and Tails'

---
+++ FlipCoin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    public class FlipCoin
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTimes;
+            Console.WriteLine("Enter number of time you want to flip the coin :");
+            numberOfTimes = int.Parse(Console.ReadLine());
+            
+            int numberOfItration = 0;
+            int numberOfHeads = 0;
+            int numberOfTails = 0;
+            while (numberOfItration <= numberOfTimes)
+            {
+                numberOfItration++;
+                Random random = new Random();
+                double coinFlip = random.NextDouble();
+                //Console.WriteLine("Check"+coinFlip);
+                
+
+                if (coinFlip<0.5)
+                {
+                    Console.WriteLine("You got Tails.");
+                    numberOfTails++;
+                }
+                else
+                {
+                    Console.WriteLine("You got Heads.");
+                    numberOfHeads++;
+                }
+
+            }
+            double percentOfHeads = numberOfHeads * 100 / numberOfTimes;
+            Console.WriteLine("Percentage of Heads is " + percentOfHeads + "%");
+            double percetOfTails = 100 - percentOfHeads;
+            Console.WriteLine("Percentage of Tails is " + percetOfTails + "%");
+        }
+    }
+}
