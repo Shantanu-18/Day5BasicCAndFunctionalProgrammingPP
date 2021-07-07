843494bba8ea573066dfed689df0af3eaf35b7bf
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-07 12:21:07 +0530

    [Shantanu] Added: 'Prime Factors calculation program.'

---
+++ primeFactorisation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace primeFactorisation
+{
+    class primeFactorisation
+    {
+        static void Main(string[] args)
+        {
+             int i = 0;
+             int num = 0;
+             int factorial = 1;
+                
+            Console.Write("Enter the number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+
+            for (i = 1; i <= num; i++)
+            {
+                factorial *= i;
+                Console.Write("The Factorial of {0} is: {1}\n", num, factorial);
+            }
+        }
+
+        
+    }
+    
+}
