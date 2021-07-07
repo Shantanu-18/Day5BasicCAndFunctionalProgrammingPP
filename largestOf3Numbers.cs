721a631f6ddf5a4e99c7f1989ee612133ad3c6d1
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-07 14:41:29 +0530

    [Shantanu] Added: 'Program to check largest of three numbers.'

---
+++ largestOf3Numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace greatestOf3Numbers
+{
+    class largestOf3Numbers
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.Write("Enter first number: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            num3 = int.Parse(Console.ReadLine());
+
+            if (num1>num2 && num1>num3)
+            {
+                Console.WriteLine("The largest number is: "+num1);
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("The largest number is: " + num2);
+            }
+            else
+            {
+                Console.WriteLine("The largest number is: " + num3);
+            }
+        }
+    }
+}
