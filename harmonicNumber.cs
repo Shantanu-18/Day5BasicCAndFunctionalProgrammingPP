e552a9d894645486bbf8eeee948150ed72d59a5f
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-07 12:08:36 +0530

    [Shantanu] Added: 'Nth Harmonic value program.'

---
+++ harmonicNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace harmonicNumber
+{
+    class harmonicNumber
+    {
+        static void Main(string[] args)
+        {
+            int N,i;
+            double sum=0;
+            Console.WriteLine("Enter value of N:");
+            N = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= N; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                sum+= 1 / (float)i;
+            }
+            Console.Write("\nSum of Series upto {0} terms : {1} ", N, sum);
+
+        }
+    }
+}
