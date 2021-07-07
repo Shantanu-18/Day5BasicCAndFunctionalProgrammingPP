681d3503b508b78605c85bca75ddf91f6946458b
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-07 12:29:30 +0530

    [Shantanu] Added: 'Quotient and Remainder computation program.'

---
+++ quotientAndRemainder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace computeQuotientAndRemainder
+{
+    class computeQuotientAndRemainder
+    {
+        static void Main(string[] args)
+        {
+            int dividend, divisor, remainder, quotient;
+            Console.WriteLine("Enter Dividend: ");
+            dividend = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Divisor: ");
+            divisor = int.Parse(Console.ReadLine());
+
+            double r = Math.DivRem(dividend, divisor, out remainder);
+            quotient = dividend / divisor;
+
+            Console.WriteLine("Quotient = "+quotient);
+            Console.WriteLine("Remainder = "+remainder);
+
+        }
+    }
+}
